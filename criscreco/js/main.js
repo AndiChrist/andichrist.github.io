7d8430190e6ef7b9dc7bb79768d894ddcad2a853
Andreas Christ <andreas.christ@codecentric.de>
2015-10-31 02:07:42 +0100

    HTML und JS etwas angepasst

--- criscreco/js/main.js
+++ criscreco/js/main.js
@@ -1,80 +1,86 @@
-$(document).ready(function(){
-    
-    $("#start").siblings().hide();
-    
-    $("#begin").click(function(){
-        $("#start").hide();
-        $("#init").show();
+$(document).ready(function () {
+
+    $("#intro").siblings().hide();
+    $("#loesung").children().hide();
+
+    $("#continue").click(function () {
+        $("#intro").hide();
+        $("#start").show();
         colorRed = false;
         suit = "";
         image = false;
     });
 
-    $("#karteJa").click(function(){
+    $("#begin").click(function () {
+        $("#start").hide();
+        $("#init").show();
+    });
+
+    $("#karteJa").click(function () {
         $("#init").hide();
         $("#roteKarte").show();
     });
 
-    $("#karteNein").click(function(){
+    $("#karteNein").click(function () {
         $("#init").hide();
         $("#keineKarte").show();
     });
 
-    $("#weiterInit").click(function(){
+    $("#weiterInit").click(function () {
         $("#keineKarte").hide();
         $("#init").show();
     });
 
-    $("#karteRot").click(function(){
+    $("#karteRot").click(function () {
         $("#roteKarte").hide();
         colorRed = true;
         $("#herzKarte").show();
-        
+
     });
 
-    $("#karteSchwarz").click(function(){
+    $("#karteSchwarz").click(function () {
         $("#roteKarte").hide();
         colorRed = false;
         $("#pikKarte").show();
     });
 
-    $("#kartePik").click(function(){
+    $("#kartePik").click(function () {
         $("#pikKarte").hide();
         suit = "spades";
         $("#bildKarte").show();
     });
 
-    $("#karteKreuz").click(function(){
+    $("#karteKreuz").click(function () {
         $("#pikKarte").hide();
         suit = "clubs";
         $("#bildKarte").show();
     });
 
-    $("#karteHerz").click(function(){
+    $("#karteHerz").click(function () {
         $("#herzKarte").hide();
         suit = "hearts";
         $("#bildKarte").show();
     });
 
-    $("#karteKaro").click(function(){
+    $("#karteKaro").click(function () {
         $("#herzKarte").hide();
         suit = "diamonds";
         $("#bildKarte").show();
     });
 
-    $("#karteBild").click(function(){
+    $("#karteBild").click(function () {
         $("#bildKarte").hide();
         image = true;
         $("#ueberlege").show();  // finden
     });
 
-    $("#karteZahl").click(function(){
+    $("#karteZahl").click(function () {
         $("div#bildKarte").hide();
         image = false;
         $("div#ueberlege").show(); // finden
     });
 
-    $("#sprachwahl").click(function(){
+    $("#sprachwahl").click(function () {
         $("#ueberlege").hide();
         language = Math.floor((Math.random() * 3) + 1);
         switch (language) {
@@ -90,50 +96,46 @@ $(document).ready(function(){
         }
     });
 
-    $("#weiterJP").click(function(){
+    $("#weiterJP").click(function () {
         $("#japanisch").hide();
         $("#finden").show();
     });
 
-    $("#weiterRU").click(function(){
+    $("#weiterRU").click(function () {
         $("#russisch").hide();
         $("#finden").show();
     });
 
-    $("#weiterEN").click(function(){
+    $("#weiterEN").click(function () {
         $("#englisch").hide();
         $("#finden").show();
     });
 
 
-    $("#weiterLoesung").click(function(){
+    $("#weiterLoesung").click(function () {
         $("#finden").hide();
-        $("#loesung div div").hide();
         if (suit === "hearts") {
             if (image)
-                $("#herzBube").show();
+                $("#loesung #herzBube").show();
             else
-                $("#herzSieben").show();
+                $("#loesung #herzSieben").show();
         } else if (suit === "diamonds") {
             if (image)
-                $("#karoKoenig").show();
+                $("#loesung #karoKoenig").show();
             else
-                $("#karoZehn").show();
+                $("#loesung #karoZehn").show();
         } else if (suit === "clubs") {
             if (image)
-                $("#kreuzKoenig").show();
+                $("#loesung #kreuzKoenig").show();
             else
-                $("div#kreuzAcht").show();
+                $("#loesung #kreuzAcht").show();
         } else if (suit === "spades") {
             if (image)
-                $("#pikDame").show();
+                $("#loesung #pikDame").show();
             else
-                $("#pikNeun").show();
+                $("#loesung #pikNeun").show();
         }
-        $("#loesung").show();
     });
-
-
 
 });
 
